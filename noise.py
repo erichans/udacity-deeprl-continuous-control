be6efc2b321a667b74504a3413572477af65ff0d
Eric Hans Silva <erichans@gmail.com>
2020-03-28 12:08:12 -0300

    feat: changing noise implementation for multiple agents with linear schedule

--- noise.py
+++ noise.py
@@ -1,21 +1,44 @@
-import numpy as np
-import random
-import copy
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
 
-class OrnsteinUhlenbeckNoise:
-    
-    def __init__(self, size, seed, mu=0., theta=.15, sigma=.2):
-        self.mu = mu * np.ones(size)
+import numpy as np
+
+class OrnsteinUhlenbeckProcess:
+    def __init__(self, size, std, theta=.15, dt=1e-2, x0=None):
         self.theta = theta
-        self.sigma = sigma
-        self.seed = random.seed(seed)
+        self.mu = 0
+        self.std = std
+        self.dt = dt
+        self.x0 = x0
+        self.size = size
         self.reset()
-        
-    def reset(self):
-        self.state = copy.copy(self.mu)
-        
+
     def sample(self):
-        x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
-        self.state = x + dx
-        return self.state
+        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + self.std() * np.sqrt(
+            self.dt) * np.random.randn(*self.size)
+        self.x_prev = x
+        return x
+
+    def reset(self):
+        self.x_prev = self.x0 if self.x0 is not None else np.zeros(self.size)
+        
+class LinearSchedule:
+    def __init__(self, start, end=None, steps=None):
+        if end is None:
+            end = start
+            steps = 1
+        self.inc = (end - start) / float(steps)
+        self.current = start
+        self.end = end
+        if end > start:
+            self.bound = min
+        else:
+            self.bound = max
+
+    def __call__(self, steps=1):
+        val = self.current
+        self.current = self.bound(self.current + self.inc * steps, self.end)
+        return val
